73bd7489a01a11ad5c42961632d5cc64235224fc
ArtByAnanik <anacherkezishvili88@gmail.com>
2026-02-03 20:38:56 -0500

    Update optical_illusion.js

--- optical_illusion.js
+++ optical_illusion.js
@@ -1,58 +1,64 @@
-let song;       
-let fft;      
+const opticalIllusion = ( p ) => {
 
-let maxDiam = 250;
-let minDiam = 10;
-let diam;       
+  let song;       
+  let fft;      
+  let maxDiam = 250;
+  let minDiam = 10;
+  let diam;       
+  let canvas; // to store the canvas variables within javascript
 
+  p.preload = function() {
+    song = p.loadSound('Aphex Twin-Xtal.mp3'); 
+  };
 
-function preload() {
-  song = loadSound('Aphex Twin-Xtal.mp3'); 
-}
+// i'm using p here because i want all of my p5 projects to be in one place like the user scrolling through the projects, therefore since javascript can't have multiple functions named just setup(), I'm giving the sketch it's label and I'm telling javascript that "p" is optical illusion. 
 
-function setup() {
- // create the canvas
-  canvas = createCanvas(600, 600);
-  // attach the canvas to the div in your HTML
-  canvas.parent("sketch-container");
-  noFill();
-  fft = new p5.FFT();
-  fft.setInput(song);
-  diam = minDiam; 
-  
-}
+  p.setup = function() {
+    canvas = p.createCanvas(600, 600);
+    canvas.parent("optical_illusion-container");
+    canvas.mousePressed(toggleSong); // you gotta click the canvas for the animation to start so i'm telling javascript to only start that when clicked on this specific canvas
 
-function draw() {
-  background(0);
+    p.noFill();
+    fft = new p5.FFT();
+    fft.setInput(song);
+    diam = minDiam; 
+  };
 
+  p.draw = function() {
+    p.background(0);
 
-  stroke(random(255), random(255), random(255));
-  strokeWeight(3);
-  
-  fft.analyze();
-  
-  if (song.isPlaying()) {
-    let bassEnergy = fft.getEnergy("bass"); 
-    diam = map(bassEnergy, 150, 300, minDiam, maxDiam);
+    // use p.random
+    p.stroke(p.random(255), p.random(255), p.random(255));
+    p.strokeWeight(3);
     
-  } else {
-    diam = minDiam;
-  }
-  
-  for (let y = 0; y < height + maxDiam; y += maxDiam / 2.1) {
-    for (let x = 0; x < width + maxDiam; x += maxDiam / 2.1) {
-      if (random(2) > 0.5) { 
-        ellipse(x, y, diam, diam);
+    fft.analyze();
+
+    if (song.isPlaying()) {
+      let bassEnergy = fft.getEnergy("bass"); 
+      // use p.map
+      diam = p.map(bassEnergy, 150, 300, minDiam, maxDiam);
+    } else {
+      diam = minDiam;
+    }
+
+    // use p.height and p.width
+    for (let y = 0; y < p.height + maxDiam; y += maxDiam / 2.1) {
+      for (let x = 0; x < p.width + maxDiam; x += maxDiam / 2.1) {
+        if (p.random(2) > 0.5) { 
+          p.ellipse(x, y, diam, diam);
+        }
       }
     }
+  };
+  
+// function that handles playing/stopping
+  function toggleSong() {
+    if (song.isPlaying()) {
+      song.stop();
+    } else {
+      song.play();
+    }
   }
-}
+};
 
-function mousePressed() {
-  if (song.isPlaying()) {
-    song.stop();
-  } else {
-    song.play();
-  }
-}
-
+new p5(opticalIllusion);
